a907fff747b4315b6300e448bdad6f2e3b6b94c7
neworderofjamie <J.C.Knight@sussex.ac.uk>
2019-02-04 17:36:18 +0000

    Nicer sky mask creation tool - database root path specified on command line

--- create_sky_mask.py
+++ create_sky_mask.py
@@ -1,7 +1,8 @@
 import cv2
+import sys
 import numpy as np
 from glob import glob
-from os import path
+from os import path, mkdir
 
 def create_sky_mask(mask, raw): 
     ground = (mask == 255) 
@@ -9,18 +10,40 @@ def create_sky_mask(mask, raw):
     sky_mask[ground] = raw[ground] 
     return sky_mask
 
+
+if len(sys.argv) < 2:
+    print("Directory of route or image grid containing unwrapped images and masks expected")
+    sys.exit(2)
+
+# Build path to unwrapped files, exit if it doesn't exist
+unwrapped_dir = path.join(sys.argv[1], "unwrapped")
+if not path.exists(unwrapped_dir):
+    print("%s does not exist" % unwrapped_dir)
+    sys.exit(1)
+
+# Build path to mask files, exit if it doesn't exist
+mask_dir = path.join(sys.argv[1], "mask")
+if not path.exists(mask_dir):
+    print("%s does not exist" % unwrapped_dir)
+    sys.exit(1)
+
+# Build path to skymask files, create if it doesn't exist
+skymask_dir = path.join(sys.argv[1], "skymask")
+if not path.exists(skymask_dir):
+    mkdir(skymask_dir)
+
 # Loop through images
-for raw_path in glob("unwrapped_image_grid/mid_day/*.jpg"):
+for raw_path in glob(path.join(unwrapped_dir,"unwrapped_*.jpg")):
     print(raw_path)
     
     # Split mask path into directory and filename
-    raw_dir, raw_filename = path.split(raw_path)
+    raw_filename = path.split(raw_path)[1]
     
     # Extract title from filename
     raw_title = path.splitext(raw_filename)[0]
 
     # Build path to mask image
-    mask_path = path.join(raw_dir, raw_title + "_mask.png")
+    mask_path = path.join(mask_dir, raw_title + "_mask.png")
     
     # Load corresponding raw and mask image
     raw_image = cv2.imread(raw_path)
@@ -29,5 +52,5 @@ for raw_path in glob("unwrapped_image_grid/mid_day/*.jpg"):
     assert mask_image is not None
     
     sky_mask = create_sky_mask(mask_image, raw_image)
-    sky_mask_path = path.join(raw_dir, raw_title + "_skymask.png")
+    sky_mask_path = path.join(skymask_dir, raw_title + "_skymask.png")
     cv2.imwrite(sky_mask_path, sky_mask)
