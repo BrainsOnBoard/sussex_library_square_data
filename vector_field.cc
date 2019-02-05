dc6705088319c7f7f410c9163217a82a2deb4f27
neworderofjamie <J.C.Knight@sussex.ac.uk>
2019-02-05 14:41:29 +0000

    Incorporated RDP library and include decimated line

--- vector_field.cc
+++ vector_field.cc
@@ -6,12 +6,39 @@
 #include "navigation/perfect_memory.h"
 #include "navigation/perfect_memory_store_raw.h"
 
+#include "psimpl.h"
+
 using namespace BoBRobotics;
 using namespace units::literals;
 using namespace units::length;
 using namespace units::angle;
 using namespace units::math;
 
+namespace
+{
+std::vector<cv::Point2i> decimateRoute(const Navigation::ImageDatabase &database)
+{
+    std::vector<float> routePointComponents;
+    routePointComponents.reserve(database.size() * 2);
+    for(const auto &r : database) {
+        routePointComponents.emplace_back(centimeter_t(r.position[0]).value());
+        routePointComponents.emplace_back(centimeter_t(r.position[1]).value());
+    }
+
+    std::vector<float> decimatedRoutePointComponents;
+    psimpl::simplify_douglas_peucker<2>(routePointComponents.cbegin(), routePointComponents.cend(),
+                                        15.0,
+                                        std::back_inserter(decimatedRoutePointComponents));
+
+    std::vector<cv::Point2i> decimatedRoutePoints;
+    decimatedRoutePoints.reserve(decimatedRoutePointComponents.size() / 2);
+    for(size_t i = 0; i < decimatedRoutePointComponents.size(); i += 2) {
+        decimatedRoutePoints.emplace_back((int)std::round(decimatedRoutePointComponents[i]),
+                                          (int)std::round(decimatedRoutePointComponents[i + 1]));
+    }
+    return decimatedRoutePoints;
+}
+}
 int main()
 {
     const cv::Size imSize(120, 25);
@@ -20,12 +47,15 @@ int main()
     Navigation::PerfectMemoryRotater<> pm(imSize);
     
     // Train perfect memory, resizing images to fit
-    Navigation::ImageDatabase route("processed_routes/route5/");
+    Navigation::ImageDatabase route("routes/route5/skymask");
     pm.trainRoute(route, true);
     std::cout << "Trained on " << pm.getNumSnapshots() << " snapshots" << std::endl;
     
+    const auto decimatedRoutePoints = decimateRoute(route);
+    cv::Mat decimatedRoutePointMat(decimatedRoutePoints, true);
+
     // Load grid
-    Navigation::ImageDatabase grid("unwrapped_image_grid/mid_day/");
+    Navigation::ImageDatabase grid("image_grids/mid_day/skymask");
     assert(grid.isGrid());
     assert(grid.hasMetadata());
     
@@ -55,7 +85,9 @@ int main()
     
     // Draw route onto image
     cv::Mat routePointsMat(routePoints, true);
-    cv::polylines(gridImage, routePointsMat, false, CV_RGB(255, 0, 0));
+    cv::polylines(gridImage, routePointsMat, false, CV_RGB(128, 128, 128));
+    cv::polylines(gridImage, decimatedRoutePointMat, false, CV_RGB(255, 255, 255));
+    bool showBadMatches = false;
     
     // Loop through grid entries
     std::vector<std::vector<float>> allDifferences;
@@ -64,7 +96,7 @@ int main()
         const centimeter_t y = g.position[1];
         
         // If snapshot is within R.O.I.
-        if(x > 120_cm && x < 1200_cm && y > 0_cm && y < 1560_cm) {
+        if(x > 240_cm && x < 1200_cm && y > 0_cm && y < 1560_cm) {
             // Load snapshot and resize
             cv::Mat snapshot = g.loadGreyscale();
             cv::resize(snapshot, snapshot, imSize);
@@ -88,12 +120,28 @@ int main()
             const centimeter_t bestSnapshotY = route[bestSnapshotIndex].position[1];
 
             // If snapshot is less than 3m away i.e. algorithm hasn't entirely failed draw line from snapshot to route
-            if(sqrt(((bestSnapshotX - x) * (bestSnapshotX - x)) + ((bestSnapshotY - y) * (bestSnapshotY - y))) < 3_m) {
+            const bool goodMatch = (sqrt(((bestSnapshotX - x) * (bestSnapshotX - x)) + ((bestSnapshotY - y) * (bestSnapshotY - y))) < 3_m);
+            if(goodMatch || showBadMatches) {
                 cv::line(gridImage, cv::Point(x.value(), y.value()), cv::Point(bestSnapshotX.value(), bestSnapshotY.value()),
-                        CV_RGB(0, 255, 0));
-                std::cout << " (good)" << std::endl;
+                        goodMatch ? CV_RGB(0, 255, 0) : CV_RGB(255, 0, 0));
+            }
+            
+            if(goodMatch) {
+                std::cout << " (good)";
             }
             std::cout << std::endl;
+            
+            /*minDifferences.clear();
+            for(size_t i = 0; i < pm.getNumSnapshots(); i++) {
+                const auto elem = std::min_element(std::cbegin(allDifferences[i]), std::cend(allDifferences[i]));
+                minDifferences.emplace(*elem, i);
+            }
+            
+            size_t i = 0;
+            auto d = minDifferences.cbegin();
+            for(;i < 100; i++, d++) {
+                std::cout << d->second << ":" << d->first << std::endl;
+            }*/
             cv::imwrite("grid_image.png", gridImage);
         }
     }
