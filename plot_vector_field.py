d3310b3b4729e47e619e9e8b0c594ecf4f64462e
neworderofjamie <J.C.Knight@sussex.ac.uk>
2019-02-25 13:54:15 +0000

    Showing tick label for each grid point is a little pointless

--- plot_vector_field.py
+++ plot_vector_field.py
@@ -19,6 +19,7 @@ output_data = np.loadtxt(argv[1], delimiter=",", skiprows=1, usecols=(0, 1, 2),
                          dtype={"names": ("x", "y", "best_heading"),
                                 "formats": (np.float, np.float, np.float)})
 
+# Split input path into directory and file title
 output_dir, output_filename = path.split(argv[1])
 output_title = path.splitext(output_filename)[0]
 
@@ -26,23 +27,17 @@ output_title = path.splitext(output_filename)[0]
 _, route_name, memory, image_input = output_title.split("_")
 
 # Load route
-coords, remaining_coords = load_route(route_name, image_input)
+coords, remaining_coords = load_route(path.join("routes", route_name), image_input)
 
 # Configure palette
 colours = sns.color_palette(n_colors=3)
 
 # Create single-column figure
-fig, axis = plt.subplots(figsize=(plot_settings.column_width, 4.0))
+fig, axis = plt.subplots(figsize=(plot_settings.column_width, 5.0))
 
 # Set axis range to match that of grid
-min_x = np.amin(output_data["x"])
-max_x = np.amax(output_data["x"])
-min_y = np.amin(output_data["y"])
-max_y = np.amax(output_data["y"])
-axis.set_xlim((min_x, max_x))
-axis.set_ylim((min_y, max_y))
-axis.set_xticks(np.arange(min_x, max_x, 120))
-axis.set_yticks(np.arange(min_y, max_y, 120))
+axis.set_xlim((np.amin(output_data["x"]), np.amax(output_data["x"])))
+axis.set_ylim((np.amin(output_data["y"]), np.amax(output_data["y"])))
 
 axis.set_xlabel("X [cm]")
 axis.set_ylabel("Y [cm]")
@@ -69,8 +64,7 @@ dir_y *= scale
 axis.arrow(first_x - dir_x, first_y - dir_y, dir_x, dir_y,
            color=colours[2], length_includes_head=True, head_width=30.0, zorder=4)
 
-
-
-
-fig.tight_layout()
+fig.tight_layout(pad=0)
+if not plot_settings.presentation:
+    fig.savefig("vector_field_" + route_name + "_" + memory + "_" + image_input + ".png")
 plt.show()
